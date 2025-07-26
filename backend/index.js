cfac2b1c201dbf21063af56c74451c1fa85c2bed
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-07-27 01:59:37 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,30 @@
-import express from "express"
-import dotenv from "dotenv"
-import connectDb from "./config/db.js"
-import cookieParser from "cookie-parser"
-import cors from "cors"
-import authRouter from "./routes/auth.route.js"
-import userRouter from "./routes/user.route.js"
-import postRouter from "./routes/post.route.js"
-import loopRouter from "./routes/loop.route.js"
-import storyRouter from "./routes/story.route.js"
-import messageRouter from "./routes/message.route.js"
-import { app, server } from "./socket.js"
-dotenv.config()
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.route.js";
+import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
+import loopRouter from "./routes/loop.route.js";
+import storyRouter from "./routes/story.route.js";
+import messageRouter from "./routes/message.route.js";
+import { app, server } from "./socket.js";
 
-const port=process.env.PORT
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
-app.use(express.json())
-app.use(cookieParser())
+dotenv.config();
 
-app.use("/api/auth",authRouter)
-app.use("/api/user",userRouter)
-app.use("/api/post",postRouter)
-app.use("/api/loop",loopRouter)
-app.use("/api/story",storyRouter)
-app.use("/api/message",messageRouter)
+const port = process.env.PORT;
 
+app.use(express.json());
+app.use(cookieParser());
 
-server.listen(port , ()=>{
-    connectDb()
-    console.log("server started")
-})
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/post", postRouter);
+app.use("/api/loop", loopRouter);
+app.use("/api/story", storyRouter);
+app.use("/api/message", messageRouter);
+
+server.listen(port, () => {
+  connectDb();
+  console.log("server started on port", port);
+});
 
