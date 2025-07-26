cd564ec98d22c91c5ff01885498475239fe5a89d
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-07-27 02:04:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
+import express from "express"; 
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
 import postRouter from "./routes/post.route.js";
@@ -8,10 +10,14 @@ import loopRouter from "./routes/loop.route.js";
 import storyRouter from "./routes/story.route.js";
 import messageRouter from "./routes/message.route.js";
 import { app, server } from "./socket.js";
-
 dotenv.config();
 
 const port = process.env.PORT;
+
+app.use(cors({
+    origin: "https://echogram-sooty.vercel.app",
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -24,7 +30,7 @@ app.use("/api/story", storyRouter);
 app.use("/api/message", messageRouter);
 
 server.listen(port, () => {
-  connectDb();
-  console.log("server started on port", port);
+    connectDb();
+    console.log("server started");
 });
 
