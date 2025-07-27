69bdb8feec09450d54b6bb62403f0d0c353d5927
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-07-27 14:02:46 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
-import LeftHome from '../components/LeftHome'
-import Feed from '../components/Feed'
-import RightHome from '../components/RightHome'
+imimport React, { useEffect } from 'react';
+import LeftHome from '../components/LeftHome';
+import Feed from '../components/Feed';
+import RightHome from '../components/RightHome';
 
 function Home() {
+  useEffect(() => {
+    const handlePopState = (event) => {
+      
+      const shouldExit = window.confirm("Do you want to exit the site?");
+      if (shouldExit) {
+      
+      } else {
+       
+        window.history.pushState(null, null, window.location.pathname);
+      }
+    };
+
+    window.history.pushState(null, null, window.location.pathname); // Push initial state
+    window.addEventListener('popstate', handlePopState);
+
+    return () => {
+      window.removeEventListener('popstate', handlePopState);
+    };
+  }, []);
+
   return (
-   <div className='w-full flex justify-center items-center'>
-    <LeftHome />
-    <Feed />
-    <RightHome />
-   </div>
-  )
+    <div className='w-full flex justify-center items-center'>
+      <LeftHome />
+      <Feed />
+      <RightHome />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
