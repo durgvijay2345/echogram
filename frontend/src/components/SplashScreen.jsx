4b505e6ed4dcbcb5241285d7365cc11a27f51a7d
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-07-28 12:03:25 +0530

    Create SplashScreen.jsx

---
+++ frontend/src/components/SplashScreen.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import favinsta from '../public/favInsta.png'; 
+
+function SplashScreen() {
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-center bg-black">
+      <img
+        src={favinsta}
+        alt="Echogram"
+        className="w-[70px] h-[70px] mb-6 animate-spin-slow"
+      />
+      <p className="text-gray-500 text-sm tracking-wide">
+        from <span className="font-semibold">Echotech</span>
+      </p>
+    </div>
+  );
+}
+
+export default SplashScreen;
