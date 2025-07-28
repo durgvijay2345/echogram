008adeac0a3aec1319699185ffe16854811ea4b9
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-07-28 12:18:40 +0530

    Update SplashScreen.jsx

--- frontend/src/components/SplashScreen.jsx
+++ frontend/src/components/SplashScreen.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import favinsta from '../public/favInsta.png'; 
+import favinsta from '../assets/favinsta.png';
 
 function SplashScreen() {
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center bg-white">
+    <div className="w-screen h-screen flex flex-col items-center justify-center bg-black">
+     
       <img
         src={favinsta}
         alt="Echogram"
-        className="w-[70px] h-[70px] mb-6 animate-spin-slow"
+        className="w-[70px] h-[70px] mb-6 spin-slow filter invert"
       />
-      <p className="text-gray-500 text-sm tracking-wide">
+      
+      <p className="text-sm shimmer-text tracking-wide">
         from <span className="font-semibold">Echotech</span>
       </p>
     </div>
