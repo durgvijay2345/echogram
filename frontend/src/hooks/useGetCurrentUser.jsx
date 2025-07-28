e06547efb241191eac92270911695da6a59932fc
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-07-29 02:51:25 +0530

    Update useGetCurrentUser.jsx

--- frontend/src/hooks/useGetCurrentUser.jsx
+++ frontend/src/hooks/useGetCurrentUser.jsx
@@ -1,29 +1,34 @@
-
 import axios from 'axios';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setFollowing, setUserData, clearUserData } from '../redux/userSlice';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setFollowing, setUserData } from '../redux/userSlice';
 import { setCurrentUserStory } from '../redux/storySlice';
 import { serverUrl } from '../App';
 
 function useGetCurrentUser() {
-  const dispatch = useDispatch();
-  const storyData = useSelector(state => state.story?.storyData);
+    const dispatch = useDispatch();
+    const [loading, setLoading] = useState(true);
 
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const result = await axios.get(`${serverUrl}/api/user/current`, { withCredentials: true });
+                dispatch(setUserData(result.data));
+                dispatch(setCurrentUserStory(result.data.story));
+                dispatch(setFollowing(result.data.following)); // If needed
+            } catch (error) {
+                console.log(error);
+                dispatch(setUserData(null));
+            } finally {
+                setLoading(false);
+            }
+        };
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const result = await axios.get(${serverUrl}/api/user/current, { withCredentials: true });
-        dispatch(setUserData(result.data));  
-        dispatch(setCurrentUserStory(result.data.story));
-      } catch (error) {
-        console.log(error);
-        dispatch(clearUserData());  
-      }
-    };
-    fetchUser();
-  }, [storyData, dispatch]);
+        fetchUser();
+    }, [dispatch]);
+
+    return loading;  
 }
 
-export default useGetCurrentUser;   
+export default useGetCurrentUser;
+
