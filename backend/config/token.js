0dae384510f4ace211268a397f13e0723d0b6c4d
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-07-29 23:12:26 +0530

    Update token.js

--- backend/config/token.js
+++ backend/config/token.js
@@ -1,18 +1,20 @@
 import jwt from "jsonwebtoken";
 
-
-
-
-const genToken = async (userId) => {
+const genToken = async (userId, tokenVersion) => {
   try {
-    const token = await jwt.sign({ userId }, process.env.JWT_SECRET, {
-      expiresIn: "10y",
-    });
+    const token = await jwt.sign(
+      { userId, tokenVersion },  
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "10y", 
+      }
+    );
     return token;
   } catch (error) {
     console.error("Error generating token:", error.message);
-    return null; // or throw error if you want to handle it at caller level
+    return null;
   }
 };
 
 export default genToken;
+
