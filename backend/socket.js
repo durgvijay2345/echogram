daae3864316c0bed876ab1a8a47e4325735b09ab
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-07-30 02:54:18 +0530

    Update socket.js

--- backend/socket.js
+++ backend/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://echogram-2gxj.vercel.app"
+  "https://echogram-nu.vercel.app"
 ];
 
 // CORS for Express
