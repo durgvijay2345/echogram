22771bf157a6305d94db0e8487ef2b222f6d3e86
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-07-30 01:01:55 +0530

    Update userSlice.js

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -1,6 +1,4 @@
 // redux/userSlice.js
-import { createSlice } from "@reduxjs/toolkit"
-
 const userSlice = createSlice({
   name: "user",
   initialState: {
@@ -10,16 +8,17 @@ const userSlice = createSlice({
     following: [],
     searchData: null,
     notificationData: [],
-    loading: true,  
+    loading: true,
+    globalDataLoaded: false, 
   },
   reducers: {
     setUserData: (state, action) => {
       state.userData = action.payload;
-      state.loading = false;  
+      state.loading = false;
     },
     clearUserData: (state) => {
       state.userData = null;
-      state.loading = false;  // <-- Logout
+      state.loading = false;
     },
     setSuggestedUsers: (state, action) => {
       state.suggestedUsers = action.payload;
@@ -39,13 +38,27 @@ const userSlice = createSlice({
     toggleFollow: (state, action) => {
       const targetUserId = action.payload;
       if (state.following.includes(targetUserId)) {
-        state.following = state.following.filter(id => id != targetUserId);
+        state.following = state.following.filter(id => id !== targetUserId);
       } else {
         state.following.push(targetUserId);
       }
+    },
+    setGlobalDataLoaded: (state, action) => {
+      state.globalDataLoaded = action.payload;
     }
   }
-})
+});
 
-export const { setUserData, clearUserData, setSuggestedUsers, setProfileData, toggleFollow, setFollowing, setSearchData, setNotificationData } = userSlice.actions;
+export const {
+  setUserData,
+  clearUserData,
+  setSuggestedUsers,
+  setProfileData,
+  toggleFollow,
+  setFollowing,
+  setSearchData,
+  setNotificationData,
+  setGlobalDataLoaded 
+} = userSlice.actions;
+
 export default userSlice.reducer;
