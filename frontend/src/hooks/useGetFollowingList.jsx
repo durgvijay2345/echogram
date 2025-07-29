c98c8a79fa5fb69a077ded73f97e60b402c0df91
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-07-29 11:19:21 +0530

    Update useGetFollowingList.jsx

--- frontend/src/hooks/useGetFollowingList.jsx
+++ frontend/src/hooks/useGetFollowingList.jsx
@@ -1,25 +1,28 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { serverUrl } from '../App'
-import { useDispatch, useSelector } from 'react-redux'
-import { setFollowing, setUserData } from '../redux/userSlice'
-import { setCurrentUserStory } from '../redux/storySlice'
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFollowing } from '../redux/userSlice';
+import { serverUrl } from '../App';
 
 function useGetFollowingList() {
-    const dispatch=useDispatch()
-    const {storyData}=useSelector(state=>state.story)
-  useEffect(()=>{
-const fetchUser=async ()=>{
-    try {
-        const result=await axios.get(`${serverUrl}/api/user/followingList`,{withCredentials:true})
-         dispatch(setFollowing(result.data))
-    } catch (error) {
-        console.log(error)
-    }
-}
-fetchUser()
-  },[storyData])
+    const dispatch = useDispatch();
+    const { userData } = useSelector(state => state.user);
+
+    useEffect(() => {
+        const fetchFollowing = async () => {
+            try {
+                const result = await axios.get(`${serverUrl}/api/user/followingList`, { withCredentials: true });
+                dispatch(setFollowing(result.data));
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        if (userData) {
+            fetchFollowing();
+        }
+    }, [userData]);
 }
 
-export default useGetFollowingList
+export default useGetFollowingList;
 
