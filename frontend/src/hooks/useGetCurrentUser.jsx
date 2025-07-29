fa15b5706e3f346100952be8304d0f045f61bf51
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-07-29 18:19:29 +0530

    Update useGetCurrentUser.jsx

--- frontend/src/hooks/useGetCurrentUser.jsx
+++ frontend/src/hooks/useGetCurrentUser.jsx
@@ -6,38 +6,38 @@ import { setCurrentUserStory } from '../redux/storySlice';
 import { serverUrl } from '../App';
 import { useNavigate ,useLocation} from 'react-router-dom';
 
-
-
 function useGetCurrentUser() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const location = useLocation();
+    const { userData } = useSelector(state => state.user);
 
     useEffect(() => {
-       
         const publicPaths = ['/signin', '/signup', '/forgot-password'];
         if (publicPaths.includes(location.pathname)) return;
 
-        const fetchUser = async () => {
-            try {
-                const result = await axios.get(`${serverUrl}/api/user/current`, { withCredentials: true });
-                
-                if (result.data) {
-                    dispatch(setUserData(result.data));
-                    dispatch(setCurrentUserStory(result.data.story));
-                } else {
+        // Only fetch if userData is null
+        if (!userData) {
+            const fetchUser = async () => {
+                try {
+                    const result = await axios.get(`${serverUrl}/api/user/current`, { withCredentials: true });
+                    if (result.data) {
+                        dispatch(setUserData(result.data));
+                        dispatch(setCurrentUserStory(result.data.story));
+                    } else {
+                        dispatch(clearUserData());
+                        navigate('/signin');
+                    }
+                } catch (error) {
+                    console.log("User Fetch Error", error);
                     dispatch(clearUserData());
                     navigate('/signin');
                 }
-            } catch (error) {
-                console.log("User Fetch Error", error);
-                dispatch(clearUserData());
-                navigate('/signin');
-            }
-        };
-
-        fetchUser();
-    }, [dispatch, navigate, location.pathname]);
+            };
+            fetchUser();
+        }
+    }, [dispatch, navigate, location.pathname, userData]);
 }
 
 export default useGetCurrentUser;
+
