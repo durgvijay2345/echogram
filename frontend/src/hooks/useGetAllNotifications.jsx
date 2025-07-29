65423d3186086c60e29c7c3841859574c87d0e00
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-07-29 11:32:35 +0530

    Update useGetAllNotifications.jsx

--- frontend/src/hooks/useGetAllNotifications.jsx
+++ frontend/src/hooks/useGetAllNotifications.jsx
@@ -1,27 +1,29 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { serverUrl } from '../App'
-import { useDispatch, useSelector } from 'react-redux'
-import { setNotificationData, setUserData } from '../redux/userSlice'
-import { setPostData } from '../redux/postSlice'
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setNotificationData } from '../redux/userSlice';
+import { serverUrl } from '../App';
 
 function useGetAllNotifications() {
-  const dispatch = useDispatch();
-const {userData}=useSelector(state=>state.user)
+    const dispatch = useDispatch();
+    const { userData } = useSelector(state => state.user);
 
-  useEffect(() => {
-    const fetchNotifications = async () => {
-      try {
-        const result = await axios.get(`${serverUrl}/api/user/getAllNotifications`, { withCredentials: true });
-        dispatch(setNotificationData(result.data));
-      } catch (error) {
-        console.error(error);
-      }
-    };
+    useEffect(() => {
+        const fetchNotifications = async () => {
+            try {
+                const result = await axios.get(`${serverUrl}/api/user/getAllNotifications`, { withCredentials: true });
+                dispatch(setNotificationData(result.data));
+            } catch (error) {
+                console.error(error);
+            }
+        };
 
-    fetchNotifications();
-  }, [dispatch,userData]);
+        if (userData) {
+            fetchNotifications();
+        }
+    }, [userData]);
 }
 
 export default useGetAllNotifications;
+
 
