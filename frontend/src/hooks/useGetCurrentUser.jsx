298d69a52ad307e5978932d8d8850641b22f11f4
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-07-29 11:34:39 +0530

    Update useGetCurrentUser.jsx

--- frontend/src/hooks/useGetCurrentUser.jsx
+++ frontend/src/hooks/useGetCurrentUser.jsx
@@ -1,15 +1,12 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { useDispatch,useSelector} from 'react-redux';
-import { setFollowing, setUserData } from '../redux/userSlice';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setUserData } from '../redux/userSlice';
 import { setCurrentUserStory } from '../redux/storySlice';
 import { serverUrl } from '../App';
 
-
 function useGetCurrentUser() {
     const dispatch = useDispatch();
-    const [loading, setLoading] = useState(true);
-      const { storyData } = useSelector(state => state.story);
 
     useEffect(() => {
         const fetchUser = async () => {
@@ -17,20 +14,15 @@ function useGetCurrentUser() {
                 const result = await axios.get(`${serverUrl}/api/user/current`, { withCredentials: true });
                 dispatch(setUserData(result.data));
                 dispatch(setCurrentUserStory(result.data.story));
-                dispatch(setFollowing(result.data.following)); // If needed
             } catch (error) {
                 console.log(error);
-                dispatch(setUserData(null));
-            } finally {
-                setLoading(false);
             }
         };
 
         fetchUser();
-    }, [dispatch,storyData]);
-
-    return loading;  
+    }, []);
 }
 
 export default useGetCurrentUser;
+
 
