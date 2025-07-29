6dfe463a85051741fe0a80f494f4a349dc117420
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-07-30 01:54:31 +0530

    Update loopSlice.js

--- frontend/src/redux/loopSlice.js
+++ frontend/src/redux/loopSlice.js
@@ -1,3 +1,21 @@
+// import { createSlice } from "@reduxjs/toolkit"
+// const loopSlice=createSlice({
+//     name:"loop",
+//     initialState:{
+//         loopData:[],
+//     },
+//     reducers:{
+//        setLoopData:(state,action)=>{
+//         state.loopData=action.payload
+//        } 
+//     }
+
+// })
+
+// export const {setLoopData}=loopSlice.actions
+// export default loopSlice.reducer
+
+
 import { createSlice } from "@reduxjs/toolkit"
 const loopSlice=createSlice({
     name:"loop",
