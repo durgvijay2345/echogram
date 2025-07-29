9e73085deb73ec1ef177a5c258363cc5c8f676f2
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-07-30 00:58:08 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,13 +7,11 @@ import { setOnlineUsers, setSocket } from './redux/socketSlice';
 import { setNotificationData } from './redux/userSlice';
 
 import useGetCurrentUser from './hooks/useGetCurrentUser';
-import useGetAllPost from './hooks/useGetAllPost';
-import useGetAllLoops from './hooks/useGetAllLoops';
-import useGetAllStories from './hooks/useGetAllStories';
 import useGetFollowingList from './hooks/useGetFollowingList';
 import useGetPrevChatUsers from './hooks/useGetPrevChatUsers';
 import useGetAllNotifications from './hooks/useGetAllNotifications';
 import useGetSuggestedUsers from './hooks/useGetSuggestedUsers';
+import useGlobalDataLoader from './hooks/useGlobalDataLoader';  
 
 // Import Pages
 import SignUp from './pages/SignUp';
@@ -39,28 +37,23 @@ import ClipLoader from "react-spinners/ClipLoader";
 export const serverUrl = "https://echogram-backend-wkov.onrender.com";
 
 function App() {
+  const { userData, notificationData, loading, globalDataLoaded } = useSelector(state => state.user);
+  const { socket } = useSelector(state => state.socket);
+  const dispatch = useDispatch();
 
-  // Custom Hooks
+  // Global User Initial Data Fetch
   useGetCurrentUser();
   useGetSuggestedUsers();
-  useGetAllPost();
-  useGetAllLoops();
-  useGetAllStories();
   useGetFollowingList();
   useGetPrevChatUsers();
   useGetAllNotifications();
-
-  const { userData, notificationData, loading } = useSelector(state => state.user);
-  const { socket } = useSelector(state => state.socket);
-  const dispatch = useDispatch();
+  useGlobalDataLoader();  // <-- Load Posts and Stories Globally here
 
   // Socket Setup
   React.useEffect(() => {
     if (userData) {
       const socketIo = io(`${serverUrl}`, {
-        query: {
-          userId: userData._id
-        }
+        query: { userId: userData._id }
       });
       dispatch(setSocket(socketIo));
 
@@ -77,11 +70,14 @@ function App() {
     }
   }, [userData]);
 
-  socket?.on("newNotification", (noti) => {
-    dispatch(setNotificationData([...notificationData, noti]));
-  });
+  React.useEffect(() => {
+    socket?.on("newNotification", (noti) => {
+      dispatch(setNotificationData([...notificationData, noti]));
+    });
+  }, [socket, notificationData]);
 
-  if (loading) {
+  // Loading Spinner till data is loaded
+  if (loading || !globalDataLoaded) {
     return (
       <div className="w-full h-screen flex justify-center items-center bg-black">
         <ClipLoader color="#fff" size={50} />
@@ -92,73 +88,21 @@ function App() {
   return (
     <Routes>
       {/* Public Routes */}
-      <Route path='/signup' element={
-        <PublicRoute>
-          <SignUp />
-        </PublicRoute>
-      } />
-      <Route path='/signin' element={
-        <PublicRoute>
-          <SignIn />
-        </PublicRoute>
-      } />
-      <Route path='/forgot-password' element={
-        <PublicRoute>
-          <ForgotPassword />
-        </PublicRoute>
-      } />
+      <Route path='/signup' element={<PublicRoute><SignUp /></PublicRoute>} />
+      <Route path='/signin' element={<PublicRoute><SignIn /></PublicRoute>} />
+      <Route path='/forgot-password' element={<PublicRoute><ForgotPassword /></PublicRoute>} />
 
       {/* Protected Routes */}
-      <Route path='/' element={
-        <ProtectedRoute>
-          <Home />
-        </ProtectedRoute>
-      } />
-      <Route path='/profile/:userName' element={
-        <ProtectedRoute>
-          <Profile />
-        </ProtectedRoute>
-      } />
-      <Route path='/editprofile' element={
-        <ProtectedRoute>
-          <EditProfile />
-        </ProtectedRoute>
-      } />
-      <Route path='/upload' element={
-        <ProtectedRoute>
-          <Upload />
-        </ProtectedRoute>
-      } />
-      <Route path='/loops' element={
-        <ProtectedRoute>
-          <Loops />
-        </ProtectedRoute>
-      } />
-      <Route path='/story/:userName' element={
-        <ProtectedRoute>
-          <Story />
-        </ProtectedRoute>
-      } />
-      <Route path='/messages' element={
-        <ProtectedRoute>
-          <Messages />
-        </ProtectedRoute>
-      } />
-      <Route path='/messageArea' element={
-        <ProtectedRoute>
-          <MessageArea />
-        </ProtectedRoute>
-      } />
-      <Route path='/search' element={
-        <ProtectedRoute>
-          <Search />
-        </ProtectedRoute>
-      } />
-      <Route path='/notifications' element={
-        <ProtectedRoute>
-          <Notifications />
-        </ProtectedRoute>
-      } />
+      <Route path='/' element={<ProtectedRoute><Home /></ProtectedRoute>} />
+      <Route path='/profile/:userName' element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+      <Route path='/editprofile' element={<ProtectedRoute><EditProfile /></ProtectedRoute>} />
+      <Route path='/upload' element={<ProtectedRoute><Upload /></ProtectedRoute>} />
+      <Route path='/loops' element={<ProtectedRoute><Loops /></ProtectedRoute>} />
+      <Route path='/story/:userName' element={<ProtectedRoute><Story /></ProtectedRoute>} />
+      <Route path='/messages' element={<ProtectedRoute><Messages /></ProtectedRoute>} />
+      <Route path='/messageArea' element={<ProtectedRoute><MessageArea /></ProtectedRoute>} />
+      <Route path='/search' element={<ProtectedRoute><Search /></ProtectedRoute>} />
+      <Route path='/notifications' element={<ProtectedRoute><Notifications /></ProtectedRoute>} />
 
       {/* Public (No auth required) */}
       <Route path='/goodbye' element={<Goodbye />} />
