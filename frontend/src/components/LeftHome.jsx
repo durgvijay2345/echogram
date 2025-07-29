464cefe057a38e818463cd9738cdc863b642b679
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-07-29 18:25:03 +0530

    Update LeftHome.jsx

--- frontend/src/components/LeftHome.jsx
+++ frontend/src/components/LeftHome.jsx
@@ -18,7 +18,7 @@ function LeftHome() {
     try {
       await axios.get(`${serverUrl}/api/auth/signout`, { withCredentials: true });
       dispatch(setUserData(null));
-        navigate("/signin")
+        
     } catch (error) {
       console.log(error);
     }
