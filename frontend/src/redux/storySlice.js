9f7a7db83f37bb92d74fae3eb34a6c09b21a89f4
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-07-30 01:57:05 +0530

    Update storySlice.js

--- frontend/src/redux/storySlice.js
+++ frontend/src/redux/storySlice.js
@@ -1,8 +1,40 @@
+// import { createSlice } from "@reduxjs/toolkit"
+// const storySlice=createSlice({
+//     name:"story",
+//     initialState:{
+//         storyData:[],
+//         storyList:[],
+//         currentUserStory:null,
+//         storyArrayData: [],
+
+//     },
+//     reducers:{
+//        setStoryData:(state,action)=>{
+//         state.storyData=action.payload
+//        } ,
+//          setStoryList:(state,action)=>{
+//         state.storyList=action.payload
+//        } 
+//        ,
+//         setCurrentUserStory:(state,action)=>{
+//         state.currentUserStory=action.payload
+//        } ,
+//        setStoryArrayData: (state, action) => {
+//     state.storyArrayData = action.payload;
+// },
+
+//     }
+
+// })
+
+// export const {setStoryData,setStoryList,setCurrentUserStory,setStoryArrayData}=storySlice.actions
+// export default storySlice.reducer
+
 import { createSlice } from "@reduxjs/toolkit"
 const storySlice=createSlice({
-    name:"story",
+    name:"post",
     initialState:{
-        storyData:[],
+        storyData:null,
         storyList:[],
         currentUserStory:null,
         storyArrayData: [],
