9469b9b15a0df36aee76fd0d48d9d58d9a8216ed
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-07-30 11:05:14 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { io } from "socket.io-client";
+import { io } from 'socket.io-client';
+import ClipLoader from 'react-spinners/ClipLoader';
 
+// Redux Actions
 import { setOnlineUsers, setSocket } from './redux/socketSlice';
 import { setNotificationData } from './redux/userSlice';
 
+// Custom Hooks (Data Fetching)
 import useGetCurrentUser from './hooks/useGetCurrentUser';
 import useGetAllPost from './hooks/useGetAllPost';
 import useGetAllLoops from './hooks/useGetAllLoops';
@@ -15,7 +18,7 @@ import useGetPrevChatUsers from './hooks/useGetPrevChatUsers';
 import useGetAllNotifications from './hooks/useGetAllNotifications';
 import useGetSuggestedUsers from './hooks/useGetSuggestedUsers';
 
-// Import Pages
+// Pages
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import ForgotPassword from './pages/ForgotPassword';
@@ -31,16 +34,15 @@ import Search from './pages/Search';
 import Notifications from './pages/Notifications';
 import Goodbye from './pages/Goodbye';
 
+// Route Protection Components
 import ProtectedRoute from './components/ProtectedRoute';
 import PublicRoute from './components/PublicRoute';
 
-import ClipLoader from "react-spinners/ClipLoader";
-
-export const serverUrl = "https://echogram-backend-wkov.onrender.com";
+// Server URL
+export const serverUrl = 'https://echogram-backend-wkov.onrender.com';
 
 function App() {
-
-  // Custom Hooks (Data Fetching)
+  // Data Fetching Hooks (Only runs when user is authenticated inside hooks)
   useGetCurrentUser();
   useGetSuggestedUsers();
   useGetAllPost();
@@ -50,18 +52,19 @@ function App() {
   useGetPrevChatUsers();
   useGetAllNotifications();
 
-  const { userData, notificationData, loading } = useSelector(state => state.user);
-  const { socket } = useSelector(state => state.socket);
   const dispatch = useDispatch();
+  const { userData, notificationData, loading } = useSelector((state) => state.user);
+  const { socket } = useSelector((state) => state.socket);
 
   // Socket.IO Setup
   useEffect(() => {
     if (userData) {
-      const socketIo = io(`${serverUrl}`, {
+      const socketIo = io(serverUrl, {
         query: {
-          userId: userData._id
-        }
+          userId: userData._id,
+        },
       });
+
       dispatch(setSocket(socketIo));
 
       socketIo.on('getOnlineUsers', (users) => {
@@ -72,34 +75,32 @@ function App() {
         socketIo.close();
         dispatch(setSocket(null));
       };
-    } else {
-      if (socket) {
-        socket.close();
-        dispatch(setSocket(null));
-      }
+    } else if (socket) {
+      socket.close();
+      dispatch(setSocket(null));
     }
-  }, [userData, dispatch]);
+  }, [userData, dispatch, socket]);
 
-  // Listen for new notifications (inside useEffect to avoid multiple listeners)
+  // Listen for New Notifications
   useEffect(() => {
     if (socket) {
       const handleNewNotification = (noti) => {
-        dispatch(setNotificationData([...notificationData, noti]));
+        dispatch(setNotificationData((prevNotis) => [...prevNotis, noti]));
       };
 
-      socket.on("newNotification", handleNewNotification);
+      socket.on('newNotification', handleNewNotification);
 
       return () => {
-        socket.off("newNotification", handleNewNotification);
+        socket.off('newNotification', handleNewNotification);
       };
     }
-  }, [socket, notificationData, dispatch]);
+  }, [socket, dispatch]);
 
-  // Loader while fetching userData
+  // Global Loader While Fetching User
   if (loading) {
     return (
       <div className="w-full h-screen flex justify-center items-center bg-black">
-        <ClipLoader color="#fff" size={50} />
+        <ClipLoader color="#ffffff" size={50} />
       </div>
     );
   }
@@ -107,83 +108,123 @@ function App() {
   return (
     <Routes>
       {/* Public Routes */}
-      <Route path='/signup' element={
-        <PublicRoute>
-          <SignUp />
-        </PublicRoute>
-      } />
-      <Route path='/signin' element={
-        <PublicRoute>
-          <SignIn />
-        </PublicRoute>
-      } />
-      <Route path='/forgot-password' element={
-        <PublicRoute>
-          <ForgotPassword />
-        </PublicRoute>
-      } />
+      <Route
+        path="/signup"
+        element={
+          <PublicRoute>
+            <SignUp />
+          </PublicRoute>
+        }
+      />
+      <Route
+        path="/signin"
+        element={
+          <PublicRoute>
+            <SignIn />
+          </PublicRoute>
+        }
+      />
+      <Route
+        path="/forgot-password"
+        element={
+          <PublicRoute>
+            <ForgotPassword />
+          </PublicRoute>
+        }
+      />
 
       {/* Protected Routes */}
-      <Route path='/' element={
-        <ProtectedRoute>
-          <Home />
-        </ProtectedRoute>
-      } />
-      <Route path='/profile/:userName' element={
-        <ProtectedRoute>
-          <Profile />
-        </ProtectedRoute>
-      } />
-      <Route path='/editprofile' element={
-        <ProtectedRoute>
-          <EditProfile />
-        </ProtectedRoute>
-      } />
-      <Route path='/upload' element={
-        <ProtectedRoute>
-          <Upload />
-        </ProtectedRoute>
-      } />
-      <Route path='/loops' element={
-        <ProtectedRoute>
-          <Loops />
-        </ProtectedRoute>
-      } />
-      <Route path='/story/:userName' element={
-        <ProtectedRoute>
-          <Story />
-        </ProtectedRoute>
-      } />
-      <Route path='/messages' element={
-        <ProtectedRoute>
-          <Messages />
-        </ProtectedRoute>
-      } />
-      <Route path='/messageArea' element={
-        <ProtectedRoute>
-          <MessageArea />
-        </ProtectedRoute>
-      } />
-      <Route path='/search' element={
-        <ProtectedRoute>
-          <Search />
-        </ProtectedRoute>
-      } />
-      <Route path='/notifications' element={
-        <ProtectedRoute>
-          <Notifications />
-        </ProtectedRoute>
-      } />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/profile/:userName"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/editprofile"
+        element={
+          <ProtectedRoute>
+            <EditProfile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/upload"
+        element={
+          <ProtectedRoute>
+            <Upload />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/loops"
+        element={
+          <ProtectedRoute>
+            <Loops />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/story/:userName"
+        element={
+          <ProtectedRoute>
+            <Story />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/messages"
+        element={
+          <ProtectedRoute>
+            <Messages />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/messageArea"
+        element={
+          <ProtectedRoute>
+            <MessageArea />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/search"
+        element={
+          <ProtectedRoute>
+            <Search />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/notifications"
+        element={
+          <ProtectedRoute>
+            <Notifications />
+          </ProtectedRoute>
+        }
+      />
 
-      {/* Public Route without Auth */}
-      <Route path='/goodbye' element={<Goodbye />} />
+      {/* Public Route (No Auth Required) */}
+      <Route path="/goodbye" element={<Goodbye />} />
 
       {/* Fallback Route */}
-      <Route path='*' element={<Navigate to='/' />} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
 
 export default App;
+
 
 
