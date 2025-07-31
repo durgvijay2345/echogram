90698a00982613ba4879be4bcbb1aae630514a3a
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-07-31 20:48:08 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    historyApiFallback: true  // ✅ This line handles refresh on dynamic routes
-  },
-  build: {
-    rollupOptions: {
-      input: '/index.html'  // Ensure fallback during build
-    }
-  }
+  plugins: [react(),tailwindcss()],
+   
 })
