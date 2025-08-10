aed24de6b74354ffa6ae2da4ef63edcaccc0df3f
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-08-10 15:44:37 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,70 +1,76 @@
-import React, { useEffect } from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { io } from "socket.io-client";
 
-import { setOnlineUsers, setSocket } from './redux/socketSlice';
-import { setNotificationData } from './redux/userSlice';
+import { setOnlineUsers, setSocket } from "./redux/socketSlice";
+import { setNotificationData } from "./redux/userSlice";
 
-import useGetCurrentUser from './hooks/useGetCurrentUser';
-import useGetAllPost from './hooks/useGetAllPost';
-import useGetAllLoops from './hooks/useGetAllLoops';
-import useGetAllStories from './hooks/useGetAllStories';
-import useGetFollowingList from './hooks/useGetFollowingList';
-import useGetPrevChatUsers from './hooks/useGetPrevChatUsers';
-import useGetAllNotifications from './hooks/useGetAllNotifications';
-import useGetSuggestedUsers from './hooks/useGetSuggestedUsers';
+// Hooks
+import useGetCurrentUser from "./hooks/useGetCurrentUser";
+import useGetAllPost from "./hooks/useGetAllPost";
+import useGetAllLoops from "./hooks/useGetAllLoops";
+import useGetAllStories from "./hooks/useGetAllStories";
+import useGetFollowingList from "./hooks/useGetFollowingList";
+import useGetPrevChatUsers from "./hooks/useGetPrevChatUsers";
+import useGetAllNotifications from "./hooks/useGetAllNotifications";
+import useGetSuggestedUsers from "./hooks/useGetSuggestedUsers";
 
-// Import Pages
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
-import ForgotPassword from './pages/ForgotPassword';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import EditProfile from './pages/EditProfile';
-import Upload from './pages/Upload';
-import Loops from './pages/Loops';
-import Story from './pages/Story';
-import Messages from './pages/Messages';
-import MessageArea from './pages/MessageArea';
-import Search from './pages/Search';
-import Notifications from './pages/Notifications';
-import Goodbye from './pages/Goodbye';
+// Pages
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import EditProfile from "./pages/EditProfile";
+import Upload from "./pages/Upload";
+import Loops from "./pages/Loops";
+import Story from "./pages/Story";
+import Messages from "./pages/Messages";
+import MessageArea from "./pages/MessageArea";
+import Search from "./pages/Search";
+import Notifications from "./pages/Notifications";
+import Goodbye from "./pages/Goodbye";
 
-import ProtectedRoute from './components/ProtectedRoute';
-import PublicRoute from './components/PublicRoute';
+// Routes
+import ProtectedRoute from "./components/ProtectedRoute";
+import PublicRoute from "./components/PublicRoute";
 
+// Loader
 import ClipLoader from "react-spinners/ClipLoader";
 
 export const serverUrl = "https://echogram-backend-wkov.onrender.com";
 
 function App() {
-
-  // Custom Hooks (Data Fetching)
-  useGetCurrentUser();
-  useGetSuggestedUsers();
-  useGetAllPost();
-  useGetAllLoops();
-  useGetAllStories();
-  useGetFollowingList();
-  useGetPrevChatUsers();
-  useGetAllNotifications();
-
-  const { userData, notificationData, loading } = useSelector(state => state.user);
-  const { socket } = useSelector(state => state.socket);
+  const { userData, notificationData, loading } = useSelector(
+    (state) => state.user
+  );
+  const { socket } = useSelector((state) => state.socket);
   const dispatch = useDispatch();
 
-  // Socket.IO Setup
+  const token = localStorage.getItem("token"); // <-- auth check
+
+  // ✅ Only fetch current user if token exists
+  if (token) {
+    useGetCurrentUser();
+    useGetSuggestedUsers();
+    useGetAllPost();
+    useGetAllLoops();
+    useGetAllStories();
+    useGetFollowingList();
+    useGetPrevChatUsers();
+    useGetAllNotifications();
+  }
+
+  // Socket Setup
   useEffect(() => {
     if (userData) {
       const socketIo = io(`${serverUrl}`, {
-        query: {
-          userId: userData._id
-        }
+        query: { userId: userData._id },
       });
       dispatch(setSocket(socketIo));
 
-      socketIo.on('getOnlineUsers', (users) => {
+      socketIo.on("getOnlineUsers", (users) => {
         dispatch(setOnlineUsers(users));
       });
 
@@ -80,7 +86,7 @@ function App() {
     }
   }, [userData, dispatch]);
 
-  // Listen for new notifications (inside useEffect to avoid multiple listeners)
+  // Notifications listener
   useEffect(() => {
     if (socket) {
       const handleNewNotification = (noti) => {
@@ -88,15 +94,14 @@ function App() {
       };
 
       socket.on("newNotification", handleNewNotification);
-
       return () => {
         socket.off("newNotification", handleNewNotification);
       };
     }
   }, [socket, notificationData, dispatch]);
 
-  // Loader while fetching userData
-  if (loading) {
+  // ✅ Show loader only if user is logged in and data is still loading
+  if (token && loading) {
     return (
       <div className="w-full h-screen flex justify-center items-center bg-black">
         <ClipLoader color="#fff" size={50} />
@@ -106,85 +111,125 @@ function App() {
 
   return (
     <Routes>
-      {/* Public Routes */}
-      <Route path='/signup' element={
-        <PublicRoute>
-          <SignUp />
-        </PublicRoute>
-      } />
-      <Route path='/signin' element={
-        <PublicRoute>
-          <SignIn />
-        </PublicRoute>
-      } />
-      <Route path='/forgot-password' element={
-        <PublicRoute>
-          <ForgotPassword />
-        </PublicRoute>
-      } />
+      {/* Public */}
+      <Route
+        path="/signup"
+        element={
+          <PublicRoute>
+            <SignUp />
+          </PublicRoute>
+        }
+      />
+      <Route
+        path="/signin"
+        element={
+          <PublicRoute>
+            <SignIn />
+          </PublicRoute>
+        }
+      />
+      <Route
+        path="/forgot-password"
+        element={
+          <PublicRoute>
+            <ForgotPassword />
+          </PublicRoute>
+        }
+      />
 
-      {/* Protected Routes */}
-      <Route path='/' element={
-        <ProtectedRoute>
-          <Home />
-        </ProtectedRoute>
-      } />
-      <Route path='/profile/:userName' element={
-        <ProtectedRoute>
-          <Profile />
-        </ProtectedRoute>
-      } />
-      <Route path='/editprofile' element={
-        <ProtectedRoute>
-          <EditProfile />
-        </ProtectedRoute>
-      } />
-      <Route path='/upload' element={
-        <ProtectedRoute>
-          <Upload />
-        </ProtectedRoute>
-      } />
-      <Route path='/loops' element={
-        <ProtectedRoute>
-          <Loops />
-        </ProtectedRoute>
-      } />
-      <Route path='/story/:userName' element={
-        <ProtectedRoute>
-          <Story />
-        </ProtectedRoute>
-      } />
-      <Route path='/messages' element={
-        <ProtectedRoute>
-          <Messages />
-        </ProtectedRoute>
-      } />
-      <Route path='/messageArea' element={
-        <ProtectedRoute>
-          <MessageArea />
-        </ProtectedRoute>
-      } />
-      <Route path='/search' element={
-        <ProtectedRoute>
-          <Search />
-        </ProtectedRoute>
-      } />
-      <Route path='/notifications' element={
-        <ProtectedRoute>
-          <Notifications />
-        </ProtectedRoute>
-      } />
+      {/* Protected */}
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/profile/:userName"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/editprofile"
+        element={
+          <ProtectedRoute>
+            <EditProfile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/upload"
+        element={
+          <ProtectedRoute>
+            <Upload />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/loops"
+        element={
+          <ProtectedRoute>
+            <Loops />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/story/:userName"
+        element={
+          <ProtectedRoute>
+            <Story />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/messages"
+        element={
+          <ProtectedRoute>
+            <Messages />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/messageArea"
+        element={
+          <ProtectedRoute>
+            <MessageArea />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/search"
+        element={
+          <ProtectedRoute>
+            <Search />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/notifications"
+        element={
+          <ProtectedRoute>
+            <Notifications />
+          </ProtectedRoute>
+        }
+      />
 
-      {/* Public Route without Auth */}
-      <Route path='/goodbye' element={<Goodbye />} />
+      
+      <Route path="/goodbye" element={<Goodbye />} />
 
-      {/* Fallback Route */}
-      <Route path='*' element={<Navigate to='/' />} />
+    
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
 
 export default App;
+
 
 
 
