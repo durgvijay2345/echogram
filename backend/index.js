c1b009ef2698f5ac486ec3ad4efe56f6ca1aaaf9
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-08-13 13:49:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -43,11 +43,7 @@ app.options("*", cors({
 
 
 
-app.options("*", cors({
-  origin: allowedOrigins,
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-}));
+
 
 
 app.use(express.json());
