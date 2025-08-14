ea06f5fd13f040fb36cef91a739605e04a1042dd
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-08-14 20:34:24 +0530

    Update Story.jsx

--- frontend/src/pages/Story.jsx
+++ frontend/src/pages/Story.jsx
@@ -1,92 +1,56 @@
-// import axios from 'axios';
-// import React, { useEffect, useState } from 'react';
-// import { useParams } from 'react-router-dom';
-// import { serverUrl } from '../App';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { setStoryArrayData } from '../redux/storySlice';
-// import StoryCard from '../components/StoryCard';
-
-// function Story() {
-//     const { userName } = useParams();
-//     const dispatch = useDispatch();
-//     const { storyArrayData } = useSelector(state => state.story);
-//     const [currentIndex, setCurrentIndex] = useState(0);
-
-//     const handleStory = async () => {
-//         try {
-//             const result = await axios.get(`${serverUrl}/api/story/getByUserName/${userName}`, { withCredentials: true });
-//             dispatch(setStoryArrayData(result.data));
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     };
-
-//     useEffect(() => {
-//         if (userName) {
-//             handleStory();
-//         }
-//     }, [userName]);
-
-//     useEffect(() => {
-//         if (storyArrayData.length > 0) {
-//             setCurrentIndex(0); // Reset to first story on load
-//         }
-//     }, [storyArrayData]);
-
-//     return (
-//         <div className='w-full h-[100vh] bg-black flex justify-center items-center'>
-//             {storyArrayData.length > 0 && (
-//                 <StoryCard
-//                     storyData={storyArrayData[currentIndex]}
-//                     currentIndex={currentIndex}
-//                     setCurrentIndex={setCurrentIndex}
-//                     total={storyArrayData.length}
-//                 />
-//             )}
-//         </div>
-//     );
-// }
-
-// export default Story;
-
-
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { serverUrl } from '../App'
-import { useDispatch, useSelector } from 'react-redux'
-import { setStoryData } from '../redux/storySlice'
-import StoryCard from '../components/StoryCard'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { serverUrl } from '../App';
+import { useDispatch, useSelector } from 'react-redux';
+import { setStoryArrayData } from '../redux/storySlice';
+import StoryCard from '../components/StoryCard';
 
 function Story() {
-    const {userName}=useParams()
-    const dispatch=useDispatch()
-    const {storyData}=useSelector(state=>state.story)
+    const { userName } = useParams();
+    const dispatch = useDispatch();
+    const { storyArrayData } = useSelector(state => state.story);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-    const handleStory=async ()=>{
-      dispatch(setStoryData(null))
+    const handleStory = async () => {
         try {
-            const result=await axios.get(`${serverUrl}/api/story/getAll/${userName}`,{withCredentials:true})
-            dispatch(setStoryData(result.data[0]))
-            console.log(storyData)
+            const result = await axios.get(`${serverUrl}/api/story/getByUserName/${userName}`, { withCredentials: true });
+            dispatch(setStoryArrayData(result.data));
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
-    }
-    useEffect(()=>{
-        if(userName){
-         handleStory()
-        }
+    };
 
-    },[userName])
-  return (
-    <div className='w-full  h-[100vh] bg-black flex justify-center items-center'>
-      <StoryCard storyData={storyData}/>
-    </div>
-  )
+    useEffect(() => {
+        if (userName) {
+            handleStory();
+        }
+    }, [userName]);
+
+    useEffect(() => {
+        if (storyArrayData.length > 0) {
+            setCurrentIndex(0); // Reset to first story on load
+        }
+    }, [storyArrayData]);
+
+    return (
+        <div className='w-full h-[100vh] bg-black flex justify-center items-center'>
+            {storyArrayData.length > 0 && (
+                <StoryCard
+                    storyData={storyArrayData[currentIndex]}
+                    currentIndex={currentIndex}
+                    setCurrentIndex={setCurrentIndex}
+                    total={storyArrayData.length}
+                />
+            )}
+        </div>
+    );
 }
 
-export default Story
+export default Story;
+
+
+
 
 
 
