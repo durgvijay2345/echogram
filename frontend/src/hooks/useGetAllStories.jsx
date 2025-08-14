db227464865a76711803b68832bb5bb65d9916da
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-08-14 20:24:23 +0530

    Update useGetAllStories.jsx

--- frontend/src/hooks/useGetAllStories.jsx
+++ frontend/src/hooks/useGetAllStories.jsx
@@ -1,30 +1,70 @@
+// import { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import axios from 'axios';
+// import { serverUrl } from '../App';
+// import { setStoryData } from '../redux/storySlice';
+
+// function useGetAllStories() {
+//     const dispatch = useDispatch();
+//     const { userData } = useSelector(state => state.user);
+//      const {storyData}=useSelector(state=>state.story)
+
+//     useEffect(() => {
+//         const fetchStories = async () => {
+//             try {
+//                 const result = await axios.get(`${serverUrl}/api/story/getAll`, { withCredentials: true });
+//                 dispatch(setStoryData(result.data));
+//             } catch (error) {
+//                 console.log(error);
+//             }
+//         };
+
+//         if (userData) { 
+//             fetchStories();
+//         }
+//     }, [userData,storyData]);  
+
+// }
+
+// export default useGetAllStories;
+
+
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 import { serverUrl } from '../App';
-import { setStoryData } from '../redux/storySlice';
+import { setStoryData, setStoryList, setCurrentUserStory } from '../redux/storySlice';
 
 function useGetAllStories() {
     const dispatch = useDispatch();
     const { userData } = useSelector(state => state.user);
-     const {storyData}=useSelector(state=>state.story)
 
     useEffect(() => {
         const fetchStories = async () => {
             try {
                 const result = await axios.get(`${serverUrl}/api/story/getAll`, { withCredentials: true });
-                dispatch(setStoryData(result.data));
+                const allStories = result.data;
+
+                dispatch(setStoryData(allStories));
+
+                // Separate current user's story
+                const myStory = allStories.find(story => story.author._id === userData._id);
+                dispatch(setCurrentUserStory(myStory || null));
+
+                // Set other users' stories
+                const otherStories = allStories.filter(story => story.author._id !== userData._id);
+                dispatch(setStoryList(otherStories));
             } catch (error) {
                 console.log(error);
             }
         };
 
-        if (userData) { 
+        if (userData) {
             fetchStories();
         }
-    }, [userData,storyData]);  
-
+    }, [userData, dispatch]);
 }
 
 export default useGetAllStories;
+
 
