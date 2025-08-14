ed7c25f8007e3ebbc5e62da5c891ea54e24a15ec
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-08-14 22:47:03 +0530

    Update useGetAllStories.jsx

--- frontend/src/hooks/useGetAllStories.jsx
+++ frontend/src/hooks/useGetAllStories.jsx
@@ -1,30 +1,28 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import axios from 'axios';
-import { serverUrl } from '../App';
-import { setStoryList } from '../redux/storySlice';
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { serverUrl } from '../App'
+import { useDispatch, useSelector } from 'react-redux'
+import { setFollowing, setUserData } from '../redux/userSlice'
+import { setStoryList } from '../redux/storySlice'
 
-function useGetAllStories() {
-    const dispatch = useDispatch();
-    const { userData } = useSelector(state => state.user);
+function  useGetAllStories() {
+    const dispatch=useDispatch()
+    const {userData}=useSelector(state=>state.user)
      const {storyData}=useSelector(state=>state.story)
-
-    useEffect(() => {
-        const fetchStories = async () => {
-            try {
-                const result = await axios.get(`${serverUrl}/api/story/getAll`, { withCredentials: true });
-                dispatch(setStoryList(result.data));
-            } catch (error) {
-                console.log(error);
-            }
-        };
-
-        if (userData) { 
-            fetchStories();
-        }
-    }, [userData,storyData]);  
-
+  useEffect(()=>{
+const fetchStories=async ()=>{
+    try {
+        const result=await axios.get(`${serverUrl}/api/story/getAll`,{withCredentials:true})
+         dispatch(setStoryList(result.data))
+         
+    } catch (error) {
+        console.log(error)
+    }
 }
+fetchStories()
+  },[userData,storyData])
+}
+
 
 export default useGetAllStories;
 
