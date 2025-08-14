1f53fc55835ede17f41fe3e4e3fb725d7e9a825d
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-08-14 18:46:40 +0530

    Update FollowButton.jsx

--- frontend/src/components/FollowButton.jsx
+++ frontend/src/components/FollowButton.jsx
@@ -2,26 +2,30 @@ import axios from 'axios'
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { serverUrl } from '../App'
-import { setFollowing, toggleFollow } from '../redux/userSlice'
+import { toggleFollow } from '../redux/userSlice'
 
-function FollowButton({targetUserId,tailwind,onFollowChange}) {
-    const {following}=useSelector(state=>state.user)
-    const isFollowing=following.includes(targetUserId)
-    const dispatch=useDispatch()
-    const handleFollow=async ()=>{
-        try {
-            const result=await axios.get(`${serverUrl}/api/user/follow/${targetUserId}`,{withCredentials:true})
-            if(onFollowChange){
-                onFollowChange()
-            }
-            dispatch(toggleFollow(targetUserId))
-        } catch (error) {
-            console.log(error)
-        }
+function FollowButton({ targetUserId, tailwind, onFollowChange }) {
+  const { following } = useSelector(state => state.user)
+  const isFollowing = following.includes(targetUserId)
+  const dispatch = useDispatch()
+
+  const handleFollow = async () => {
+    
+    dispatch(toggleFollow(targetUserId))
+    if (onFollowChange) onFollowChange()
+
+    try {
+      await axios.get(`${serverUrl}/api/user/follow/${targetUserId}`, { withCredentials: true })
+    } catch (error) {
+      console.log(error)
+      // Revert on error
+      dispatch(toggleFollow(targetUserId))
     }
+  }
+
   return (
     <button className={tailwind} onClick={handleFollow}>
-{isFollowing?"Following":"Follow"}
+      {isFollowing ? "Following" : "Follow"}
     </button>
   )
 }
